5def9e6229ccdf78cee679b832ce7acfdb64f565
Md Jubayer <mdjubayerislamshuvo34@gmail.com>
2025-11-22 01:51:31 +0600

    redgt

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 
 export default function Home() {
   return (
-    <div>
-      <h1>Under Construction</h1>
+    <div className="relative h-screen flex items-center justify-center">
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <h1 className="text-center">Under Construction</h1>
+      </div>
     </div>
   );
 }
