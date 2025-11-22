c9d05cccb2e0133e964ec4050e5024bf65cfe4c3
Md Jubayer <mdjubayerislamshuvo34@gmail.com>
2025-11-22 15:15:36 +0600

    wfff

--- app/reseller/wallet/page.tsx
+++ app/reseller/wallet/page.tsx
@@ -1,11 +1,5 @@
 import WalletPage from "@/components/reseller/Wallet";
-import { connectDB } from "@/lib/mongodb";
 
 export default async function ResellerWallet() {
-    const db = await connectDB();
-    const data = await db.collection("datas").findOne();
-    const walletData = {
-        percentage: data?.percentage
-    }
-    return <WalletPage data={walletData} />
+    return <WalletPage />
 }
