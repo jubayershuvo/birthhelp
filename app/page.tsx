ef74e246c2800f3f17c3d3df542b2e3a3bf7799c
Md Jubayer <mdjubayerislamshuvo34@gmail.com>
2025-11-22 12:50:00 +0600

    da

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className="relative h-screen flex items-center justify-center">
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <h1 className="text-center">Under Construction</h1>
+        <h1 className="text-center">Under Development</h1>
       </div>
     </div>
   );
