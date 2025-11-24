2f4b769f877da9b47b2ce995c04ebe997cff4744
Md Jubayer <mdjubayerislamshuvo34@gmail.com>
2025-11-24 18:01:39 +0600

    dad

--- app/api/birth/certificate/find/route.ts
+++ app/api/birth/certificate/find/route.ts
@@ -11,48 +11,42 @@ export async function POST(req: Request) {
     );
   }
 
-  try {
-    await connectDB();
-    const user = await getUser();
-    if (!user) {
-      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-    }
-
-    try {
-      const response = await fetch(
-        "https://api.fortest.top/birth_test/api/birth_verification.php",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            username: "sagarmandal712103@gmail.com",
-            password: "sagarmandal712103@gmail.com",
-            ubrn,
-            birth_date: dob,
-          }),
-        }
-      );
-
-      if (!response.ok) {
-        return NextResponse.json(
-          { success: false, error: "Failed to verify birth certificate" },
-          { status: 404 }
-        );
-      }
-
-      const jsonData = await response.json();
-      if (jsonData.success === false) {
-        return NextResponse.json(
-          { success: false, error: jsonData.error || "No data found" },
-          { status: 404 }
-        );
-      }
-    } catch (error) {
-      return NextResponse.json({ error: "faild to get data" }, { status: 500 });
-    }
-  } catch (error) {
-    return NextResponse.json({ error: "Server error" }, { status: 500 });
+  await connectDB();
+  const user = await getUser();
+  if (!user) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
   }
+
+  const response = await fetch(
+    "https://api.fortest.top/birth_test/api/birth_verification.php",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: "sagarmandal712103@gmail.com",
+        password: "sagarmandal712103@gmail.com",
+        ubrn,
+        birth_date: dob,
+      }),
+    }
+  );
+
+  if (!response.ok) {
+    return NextResponse.json(
+      { success: false, error: "Failed to verify birth certificate" },
+      { status: 404 }
+    );
+  }
+
+  const jsonData = await response.json();
+  if (jsonData.success === false) {
+    return NextResponse.json(
+      { success: false, error: jsonData.error || "No data found" },
+      { status: 404 }
+    );
+  }
+
+  return NextResponse.json(jsonData, { status: 200 });
 }
