13bd9b2a575c92d7fc9ab3ac81e366bb21c80d0b
Md Jubayer <mdjubayerislamshuvo34@gmail.com>
2025-11-24 20:05:01 +0600

    rw

--- app/api/birth/certificate/find/route.ts
+++ app/api/birth/certificate/find/route.ts
@@ -1,52 +1,62 @@
+import axios from "axios";
 import { getUser } from "@/lib/getUser";
 import { connectDB } from "@/lib/mongodb";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-  const { ubrn, dob } = await req.json();
-  if (!ubrn || !dob) {
-    return NextResponse.json(
-      { error: "All fields are required" },
-      { status: 400 }
-    );
-  }
+  try {
+    const { ubrn, dob } = await req.json();
 
-  await connectDB();
-  const user = await getUser();
-  if (!user) {
-    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-  }
-
-  const response = await fetch(
-    "https://api.fortest.top/birth_test/api/birth_verification.php",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        username: "sagarmandal712103@gmail.com",
-        password: "sagarmandal712103@gmail.com",
-        ubrn,
-        birth_date: dob,
-      }),
+    if (!ubrn || !dob) {
+      return NextResponse.json(
+        { error: "All fields are required" },
+        { status: 400 }
+      );
     }
-  );
 
-  if (!response.ok) {
+    await connectDB();
+    const user = await getUser();
+
+    if (!user) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    let apiResponse;
+
+    try {
+      apiResponse = await axios.post(
+        "https://api.fortest.top/birth_test/api/birth_verification.php",
+        {
+          username: "sagarmandal712103@gmail.com",
+          password: "sagarmandal712103@gmail.com",
+          ubrn,
+          birth_date: dob,
+        },
+        {
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      if (apiResponse.data.error || !apiResponse.data.success) {
+        return NextResponse.json(
+          { success: false, error: apiResponse.data.error },
+          { status: 400 }
+        );
+      }
+    } catch (error) {
+      return NextResponse.json(
+        {
+          success: false,
+          error: error || "External API error",
+        },
+        { status: 500 }
+      );
+    }
+
+    return NextResponse.json(apiResponse.data, { status: 200 });
+  } catch (error) {
     return NextResponse.json(
-      { success: false, error: "Failed to verify birth certificate" },
-      { status: 404 }
+      { success: false, error: error || "Server Error" },
+      { status: 500 }
     );
   }
-
-  const jsonData = await response.json();
-  if (jsonData.success === false) {
-    return NextResponse.json(
-      { success: false, error: jsonData.error || "No data found" },
-      { status: 404 }
-    );
-  }
-
-  return NextResponse.json(jsonData, { status: 200 });
 }
