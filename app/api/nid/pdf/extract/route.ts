eaff1c557024c2041218c2f4447ed25669788dd7
Md Jubayer <mdjubayerislamshuvo34@gmail.com>
2026-03-16 16:37:09 +0600

    asaS

--- app/api/nid/pdf/extract/route.ts
+++ app/api/nid/pdf/extract/route.ts
@@ -93,10 +93,10 @@ export async function POST(req: Request) {
       return `${day} ${month} ${year}`; // No comma
     }
 
-    const barcodeData = `<pin>${pdfResult.data.pincode}</pin><name>${
-      pdfResult.data.name_en
+    const barcodeData = `<pin>${pdfResult.data.pin}</pin><name>${
+      pdfResult.data.nameEnglish
     }</name><DOB>${formatDate(
-      pdfResult.data.dob,
+      pdfResult.data.dateOfBirth,
     )}</DOB><FP></FP><F>Right Index</F><TYPE>A</TYPE><V>2.0</V><ds>302c02167da6b272e960dfaf8a7ccca6b031da99defe8d24c44882580f7a9b3fea93b99040f65c34e8edafe9de63</ds>`;
     // Save data to MongoDB
 
@@ -124,7 +124,9 @@ export async function POST(req: Request) {
       fs.mkdirSync(imageDirPath, { recursive: true });
 
     async function urlToBuffer(photoUrl: string) {
-      const response = await axios.get(photoUrl, { responseType: "arraybuffer" });
+      const response = await axios.get(photoUrl, {
+        responseType: "arraybuffer",
+      });
       return Buffer.from(response.data, "binary");
     }
 
